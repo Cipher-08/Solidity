54e50a4b48e8138bb5ef7e0ad26b24ce43dba4ca
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2022-03-31 01:04:27 +0530

    Create StateVariable.sol

---
+++ StateVariable.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract StateVariable{
+   
+        uint public number;
+
+        uint public age =18;
+        function setNumber() public{
+            number = 100;
+        }
+
+        
+        
+    
+
+}
